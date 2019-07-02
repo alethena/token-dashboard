e4d8b6a14f555cd59e74aa9160fe19f1c1dc8d86
pcaversaccio <pascal.caversaccio@hotmail.ch>
2019-07-02 15:47:24 +0200

    Fixed Web3 Fallback & Edge compatibility issue.

--- src/app/services/metamask/web3.service.ts
+++ src/app/services/metamask/web3.service.ts
@@ -1,46 +1,50 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import contract from 'truffle-contract';
+import { MatSnackBar } from '@angular/material';
 import { DataService } from '../data/data.service';
+
 declare let require: any;
-
 const InfuraWebsocket = 'wss://rinkeby.infura.io/ws/v3/506b137aa0d543268e847d6affb7963c';
-
 const Web3 = require('web3');
-
 declare let window: any;
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class Web3Service {
   public web3: any;
+  public MM: any;
 
-  constructor(private dataService: DataService) {
+  constructor(private matSnackBar: MatSnackBar) {
     window.addEventListener('load', (event) => {
-        this.bootstrapWeb3();
-      });
-   }
-
-  public async bootstrapWeb3() {
-    return new Promise(async (resolve, reject) => {
-      if (typeof window.ethereum !== 'undefined') {
-        try {
-          await window.ethereum.enable();
-        } catch (error) {
-          reject('User rejected connection');
-        }
-      } if ((typeof window.ethereum !== 'undefined') || (typeof window.web3 !== 'undefined')) {
-        const provider = window['ethereum'] || window.web3.currentProvider;
-        this.web3 = new Web3(provider);
-        this.dataService.web3Observable.next(this.web3);
-        resolve();
-      } else {
-        reject('MetaMask not available');
-      }
+      this.bootstrapWeb3();
     });
   }
-  
+
+  setStatus(status) {
+    this.matSnackBar.open(status, null, { duration: 6000, verticalPosition: 'top' });
+  }
+
+  public async bootstrapWeb3() {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof window.ethereum !== 'undefined') {
+      try {
+        this.MM = await window.ethereum.enable();
+      } catch (error) { }
+    }
+    if ((typeof window.ethereum !== 'undefined') || (typeof window.web3 !== 'undefined')) {
+      const provider = window['ethereum'] || window.web3.currentProvider;
+      this.web3 = new Web3(provider);
+      Web3.providers.WebsocketProvider.prototype.sendAsync = Web3.providers.WebsocketProvider.prototype.send;
+
+      // Hack to provide backwards compatibility for Truffle, which uses web3js 0.20.x
+      // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail);
+    } else {
+      Web3.providers.WebsocketProvider.prototype.sendAsync = Web3.providers.WebsocketProvider.prototype.send;
+
+      this.web3 = new Web3(new Web3.providers.WebsocketProvider(InfuraWebsocket));
+    }
+  }
+
   public async artifactsToContract(artifacts) {
     if (!this.web3) {
       const delay = new Promise(resolve => setTimeout(resolve, 100));
@@ -53,4 +57,21 @@ export class Web3Service {
     return contractAbstraction;
   }
 
+  public async createBatch() {
+    return await this.web3.createBatch();
+  }
+
+  public async toBigNumber(number) {
+    return this.web3.utils.toBN(number);
+  }
+
+  public async getAccounts() {
+    // console.log(this.web3);
+    let out;
+    if (this.web3) {
+      out = await this.web3.eth.getAccounts();
+    }
+    // console.log(out);
+    return out;
+  }
 }
