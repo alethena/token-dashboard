4a78fe22a6e1f8be0868f707148210d892550518
pcaversaccio <pascal.caversaccio@hotmail.ch>
2019-07-09 22:29:13 +0200

    Added HashLocationStrategy to prevent 404 Error in case of refresh.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes, { enableTracing: true })],
+  imports: [ RouterModule.forRoot(routes, { enableTracing: true, useHash: true })],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
