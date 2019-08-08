62b050e747c1547d560552c23148c0797fa09463
pcaversaccio <pascal.caversaccio@hotmail.ch>
2019-08-08 18:32:04 +0200

    Added SD contract and implemented first function calls in Share Dispenser frontend.

--- helpers/artifactorScript.js
+++ helpers/artifactorScript.js
@@ -3,17 +3,24 @@ const Artifactor = require('truffle-artifactor');
 const artifactor = new Artifactor(__dirname);
 
 //const XCHFabi = require('../src/assets/contracts/CryptoFranc.json');
-const TTEabi = require('./TestTokenEquity.json')
+// const TTEabi = require('./TestTokenEquity.json');
+const SDabi = require ('./ShareDispenser.json');
 
 //const XCHFData = {
 //    contractName: 'XCHF',
 //    abi: XCHFabi
 //}
 
-const TTEData = {
-    contractName: 'TTE',
-    abi: TTEabi
+// const TTEData = {
+//     contractName: 'TTE',
+//     abi: TTEabi
+// }
+
+const SDData = {
+    contractName: 'SD',
+    abi: SDabi
 }
 
-//artifactor.save(XCHFData);
-artifactor.save(TTEData);
+// artifactor.save(XCHFData);
+// artifactor.save(TTEData);
+artifactor.save(SDData);
